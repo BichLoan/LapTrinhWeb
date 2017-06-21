3e9e7de332418b40bbf02b50af0617972230364a
Bich Loan <loan.ntb@anlab.pro>
2017-06-21 12:56:34 +0700

    Dang nhap tai khoan

--- LogIn.php
+++ LogIn.php
@@ -1,10 +1,12 @@
-<?php 
-	if(!isset($_COOKIE["username"]) && !isset($_COOKIE["pw"])) {
-		include('Bai1.php');
-		exit;;
-	}
-	else {
-		include('Connect.php');
-		exit;;
-	}
-?>
+<!DOCTYPE html>
+<html>
+	<body>
+		<form action = "Connect.php" method = "post">
+			User name:<br>
+			<input type = "text" name = "username"><br>
+			User password:<br>
+			<input type = "password" name = "pw"><br>
+			<input type = "submit" name = "ok" value = "Log-in"><br>
+		</form>
+	</body>
+</html>

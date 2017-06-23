e48cfe71f7eb05cb05291d316548e215f4cd0f59
Bich Loan <loan.ntb@anlab.pro>
2017-06-23 11:40:15 +0700

    Dang ky tai khoan

--- DangKy.php
+++ DangKy.php
@@ -1,53 +1,122 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Lap trinh web</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js" type="text/javascript"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
+		<script src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.js" type="text/javascript"></script>
+	</head>
 	<body>
-		<form method = "post">
+		<div class = "signin-form">
+		<form class = "form-signin" method = "POST" id = "check" name = "check">
+			<h2 class="form-signin-heading">Sign Up</h2><hr />
+            <div id="error">
+            </div>
+			<div>
 			User name:<br>
-			<input type = "text" name = "username"><br>
+			<input type = "text" name = "username" id="username" size="30"><br>
+			</div>
+			
+			<div>
 			User password:<br>
-			<input type = "password" name = "pw"><br>
+			<input type = "password" name = "pw" id = "pw"><br>
+			</div>
+			
+			<div>
 			ReConfirmPassword:<br>
-			<input type = "password" name = "confirmPW"><br>
-			<input type = "submit" name = "okie" value = "Sign-up"><br>
-			<?php
-				if (isset($_POST['okie']))
-				{      
-					$user = $pass = $cofirm = "";
-					if($_POST["username"] == NULL){
-						echo "Please enter your username<br />";
-					}
-					else{
-						$user = $_POST['username'];
-					}
-					if($_POST['pw'] == NULL){
-						echo "Please enter your password<br />";
-					}
-					else{
-						$pass = $_POST['pw'];
-					}
-					if($_POST['confirmPW'] == NULL){
-						echo "Please enter your password<br />";
-					}
-					else{
-						$pass = $_POST['confirmPW'];
-					}
-					$username = "root";
-					$password = "";
-					$host = "localhost";
-					$database = "sinhvien";
-					$connection = mysql_connect($host, $username, $password);
-					mysql_select_db($database, $connection);
-					mysql_query("SET NAMES 'UTF8'");
-
-					$name=$_POST['username'];                
-					$pass=$_POST['pw'];
-					mysql_query("insert  into `students`(`id`,`name`) values ('$pass','$name')"); 
-					
-					mysql_close($connection);
-					echo "Dang ky thanh cong";
-				}
-						
-			?>
+			<input type = "password" name = "confirmPW" id = "confirmPW"><br>
+			</div>
+			<button type = "submit" name = "sign-up" id = "btn-submit"><br>
+				<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account
+			</button>
 		</form>
+		</div>
 	</body>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			 $("#check").validate({
+				rules: {
+					username: {
+						required: true,
+					},
+					pw:{
+						required: true,
+						minlength: 4
+					},
+					confirmPW:{
+						required: true,
+						minlength: 4,
+						equalTo: "#pw"
+					}		
+				},
+				messages: {
+					username: "Không được để trống",
+					pw: {
+						required: "Không được để trống",
+						minlength: "Mật khẩu phải dài hơn 4 kí tự"
+					},
+					confirmPW: {
+						required: "Không được để trống",
+						minlength: "Mật khẩu phải dài hơn 4 kí tự",
+						equalTo: "Hai mật khẩu phải trùng nhau"
+					}
+				},
+				submitHandler: submitForm
+				});
+		});
+		function submitForm()
+		{
+			var data = $("#check").serialize();
+
+			$.ajax({
+
+				type : 'POST',
+				url  : 'kiemtra.php',
+				data : data,
+				beforeSend: function()
+				{
+					$("#error").fadeOut();
+					$("#btn-submit").html('<span class="glyphicon glyphicon-transfer"></span> &nbsp; sending ...');
+				},
+				success :  function(data)
+				{
+					if(data=="YES"){
+
+						$("#error").fadeIn(1000, function(){
+
+
+							$("#error").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; Sorry username already taken !</div>');
+
+							$("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account');
+
+						})
+
+					}
+					else if(data = "NO")
+					{
+
+						$("#btn-submit").html('Signing Up');
+						setTimeout('$(".form-signin").fadeOut(500, function(){ $(".signin-form").html("Success Sign-up"); }); ',5000);
+
+					}
+					else{
+
+						$("#error").fadeIn(1000, function(){
+
+							$("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+data+' !</div>');
+
+							$("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account');
+
+						});
+
+					}
+				}
+			});
+			return false;
+		}
+		
+	</script>
 </html>
